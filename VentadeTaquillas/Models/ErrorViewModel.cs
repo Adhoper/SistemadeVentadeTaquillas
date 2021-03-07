b9e8c74c428082925b89bda58e060b9e3db5ef88
Adrian Curet <curetmicael@gmail.com>
2021-03-07 11:49:08 -0400

    Add project files.

---
+++ VentadeTaquillas/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace VentadeTaquillas.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
