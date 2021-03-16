e926f840ab7180c32b572c06c3351cc310bd4b47
Adrian Curet <curetmicael@gmail.com>
2021-03-16 16:22:09 -0400

    Nosotros

--- VentadeTaquillas/Controllers/HomeController.cs
+++ VentadeTaquillas/Controllers/HomeController.cs
@@ -29,6 +29,11 @@ namespace VentadeTaquillas.Controllers
             return View();
         }
 
+        public IActionResult Nosotros()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
